839d86db2dd3d05298c18e4d20b0c4941bfd5b98
Anton Shepitko <antonshepitko99@gmail.com>
2024-01-24 21:37:21 +0300

    component tests

--- tests/integration.py
+++ tests/integration.py
@@ -6,19 +6,6 @@ from datetime import datetime
 from time import sleep
 from uuid import uuid4
 
-station_url = 'https://localhost:8000'
-ticket_url = 'https://localhost:8001'
-add_train_url = f'{station_url}/add_train'
-get_train_by_id_url = f'{station_url}/get_train_by_id/'
-
-train_data = {
-    "id": str(uuid4()),
-    "model": "TrainModel123",
-    "direction": "SomeDirection",
-    "departure_date": str(datetime.now()),
-    "remaining_seats": 50
-}
-
 
 def check_connect():
     try:
@@ -43,15 +30,15 @@ class TestIntegration(unittest.TestCase):
         self.assertEqual(check_connect(), True)
 
     def test_station_service_connection(self):
-        r = requests.get(f"{station_url}/health")
+        r = requests.get("https://localhost:8000/health")
         self.assertEqual(r.status_code, 200)
 
     def test_ticket_service_connection(self):
-        r = requests.get(f"{ticket_url}/health")
+        r = requests.get("https://localhost:8001/health")
         self.assertEqual(r.status_code, 200)
 
     def test_get_train(self):
-        res = requests.get(f"{get_train_by_id_url}/86f053a0-0dd1-4439-ba43-bdf586220bd2")
+        res = requests.get(f"https://localhost:8000/get_train_by_id/86f053a0-0dd1-4439-ba43-bdf586220bd2")
         res = json.loads(res.text)[0]
         pytest.assume(res['model'] == 'Test')
         pytest.assume(res['direction'] == 'St. Petersburg')
